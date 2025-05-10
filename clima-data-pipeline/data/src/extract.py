6ad5359bd3aa88d821c8f10d282b227be54a0539
Mateus Takashi Kochi <89741576+Matkochi@users.noreply.github.com>
2025-05-10 19:35:26 -0300

    Create extract.py

---
+++ clima-data-pipeline/data/src/extract.py
@@ -0,0 +1,7 @@
+# extract.py
+import requests
+
+def get_weather_data(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    return response.json()
